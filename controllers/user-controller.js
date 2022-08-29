e7ebb037c71956513c1cb10f5de6c2c9ea2cd9d9
WIlson <eaz73727@gmail.com>
2022-08-30 02:52:06 +0800

    feat:set up logout routes and function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -44,6 +44,13 @@ const userController = {
 
       })
       .catch(err => console.log(err))
+  },
+  logout: (req, res, next) => {
+    req.logout(err => {
+      if (err) return next(err)
+      req.flash('success_msg', '您已成功登出')
+      res.redirect('/users/login')
+    })
   }
 }
 
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user-controller')
 
+router.get('/logout', userController.logout)
 router.post('/login', userController.postLogin)
 router.get('/login', userController.pageLogin)
 router.get('/register', userController.pageRegister)
