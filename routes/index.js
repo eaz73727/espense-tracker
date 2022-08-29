c98bd95ec8ee44d7894680d3479be546f57d7ab4
WIlson <eaz73727@gmail.com>
2022-08-30 03:38:54 +0800

    feat: add middleware for block unauthenticated client

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,11 @@ const express = require('express')
 const router = express.Router()
 const expenseTracker = require('./modules/expense-tracker')
 const user = require('./modules/user')
+const { authenticator } = require('../middleware/auth')
 
 router.use('/users', user)
-router.use('/tracker', expenseTracker)
+router.use('/tracker', authenticator, expenseTracker)
 
-router.use('/', (req, res) => {
-  res.redirect('/tracker')
-})
+router.use('/', (req, res) => res.redirect('/tracker'))
 
 module.exports = router
