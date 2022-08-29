5b13ca37334575e0a72a87422194c021f3bdc4a2
WIlson <eaz73727@gmail.com>
2022-08-30 02:07:51 +0800

    feat: set up register & login post method and passport authenticate

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,46 @@
+const passport = require("passport")
+const User = require("../models/user")
+
 const userController = {
   pageLogin: (req, res) => {
     res.render('login')
   },
   pageRegister: (req, res) => {
     res.render('register')
+  },
+  postLogin: passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  }),
+  postRegister: (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    const errors = []
+    if (!name || !email || !password || !confirmPassword) {
+      errors.push({ message: '所有欄位都是必填！' })
+    }
+    if (password !== confirmPassword) {
+      errors.push({ message: '密碼與確認密碼不符！' })
+    }
+    if (errors.length) {
+      return res.render('register', { errors, email, name })
+    }
+    User.findOne({ email })
+      .then(user => {
+        if (user) {
+          res.render('register', { name, email })
+        } else {
+          User.create({
+            name,
+            email,
+            password
+          })
+            .then(() => {
+              res.redirect('/users/login')
+            })
+        }
+
+      })
+      .catch(err => console.log(err))
   }
 }
 
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user-controller')
 
+router.post('/login', userController.postLogin)
 router.get('/login', userController.pageLogin)
 router.get('/register', userController.pageRegister)
+router.post('/register', userController.postRegister)
 
 module.exports = router
