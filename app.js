aac29218c75656d2dc64f4dc0a6a45dc63388e60
WIlson <eaz73727@gmail.com>
2022-08-30 01:20:17 +0800

    feat: install & set up express-session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -11,6 +12,12 @@ const PORT = process.env.PORT
 
 app.engine('hbs', exphbs.engine({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(routes)
 
